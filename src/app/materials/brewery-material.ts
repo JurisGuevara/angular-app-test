0e9edf64adf15e4ef888943a55e7f401b6358dd8
unknown <tertullanojuris@gmail.com>
2021-04-28 12:07:30 +0800

    third commit

--- src/app/materials/brewery-material.ts
+++ src/app/materials/brewery-material.ts
@@ -1,11 +1,26 @@
 // custom made material module
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-    imports: [MatButtonModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatCheckboxModule],
+    imports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatCheckboxModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatMenuModule
+    ],
 })
 
 export class CustomMaterialModule {}
